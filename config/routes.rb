576eef8a27b9427ef438d56e3de329a90d9459ee
taromaro <taromaro175@gmail.com>
2025-03-24 18:37:10 +0900

    3章のためにuserとmicropostを消去

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :microposts
-  resources :users
-  root "users#index"
-  #root "application#goodbye"
+  get 'static_pages/home'
+  get 'static_pages/help'
+  root "application#goodbye"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_20_124526) do
-  create_table "microposts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.text "content"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.1].define(version: 2025_03_20_122309) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
