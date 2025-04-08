58fc46f156ee4ff73f2f63af9b98e00c48f6deef
taromaro <taromaro175@gmail.com>
2025-04-08 21:31:41 +0900

    9章途中

--- railsapp/app/controllers/sessions_controller.rb
+++ railsapp/app/controllers/sessions_controller.rb
@@ -5,8 +5,9 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password])
       reset_session
+      remember user
       log_in user
       redirect_to user
     else
