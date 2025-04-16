6f8000330484b91fdc738a313ef748e4e9c6177a
taromaro <taromaro175@gmail.com>
2025-04-16 21:02:50 +0900

    10.43まで完了

--- railsapp/db/schema.rb
+++ railsapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_08_100551) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_16_115938) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_08_100551) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
