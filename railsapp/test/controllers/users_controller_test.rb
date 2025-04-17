e4ecafb6cf0a99092b52938f4a807990b390a456
taromaro <taromaro175@gmail.com>
2025-04-17 20:51:03 +0900

    10章終了

--- railsapp/test/controllers/users_controller_test.rb
+++ railsapp/test/controllers/users_controller_test.rb
@@ -41,4 +41,21 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get users_path
     assert_redirected_to login_url
   end
+  
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_response :see_other
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_response :see_other
+    assert_redirected_to root_url
+  end
 end
