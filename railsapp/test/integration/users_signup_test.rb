fb4973517c51dc05c19afa788a588a3ab04e50a9
taromaro <taromaro175@gmail.com>
2025-04-22 20:58:20 +0900

    11.36まで終了

--- railsapp/test/integration/users_signup_test.rb
+++ railsapp/test/integration/users_signup_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
-  test "invalid signup information" do
+  test "invalidsignup information" do
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, params: { user: { name:  "",
