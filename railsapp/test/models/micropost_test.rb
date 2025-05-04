b6a2c986e7d33bc4998a32fd5b0a9fb6b74c1866
taromaro <taromaro175@gmail.com>
2025-05-04 23:23:42 +0900

    13.20終わり

--- railsapp/test/models/micropost_test.rb
+++ railsapp/test/models/micropost_test.rb
@@ -16,4 +16,8 @@ class MicropostTest < ActiveSupport::TestCase
     @micropost.user_id = nil
     assert_not @micropost.valid?
   end
+  
+  test "order should be most recent first" do
+    assert_equal microposts(:most_recent), Micropost.first
+  end
 end
